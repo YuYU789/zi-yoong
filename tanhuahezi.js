00dceea5ddcd64baa5cc89b668374f85073f2593
yu475487880 <77768638+yu475487880@users.noreply.github.com>
2021-01-26 20:28:02 +0800

    Update tanhuahezi.js

--- tanhuahezi.js
+++ tanhuahezi.js
@@ -1,4 +1,15 @@
-var body=$response.body;
-body =body.replace（/"is_vip\":0/g，'is_vip\":1'）；
+var body = {
+  "status" : "200",
+  "msg" : "返回成功",
+  "data" : {
+    "mobile" : "18310605028",
+    "status" : 1,
+    "is_vip" : 1,
+    "member_name" : "yu475487880",
+    "head_pic" : "https://s3.ax1x.com/2021/01/26/sOLQKA.jpg",
+    "parent_id" : 22,
+    "user_viptime" : "2266-06-06"
+  }
+};
 
-$done({body: body});
+$done({body:JSON.stringify(body)});
