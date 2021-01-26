651beb4c899cc02e0b6bd8ce691236158fbc8d35
yu475487880 <77768638+yu475487880@users.noreply.github.com>
2021-01-26 20:50:18 +0800

    Update tanhuahezi.js

--- tanhuahezi.js
+++ tanhuahezi.js
@@ -5,7 +5,7 @@ var body = {
     "mobile" : "18310565258",
     "status" : 1,
     "is_vip" : 1,
-    "member_name" : "12345678",
+    "member_name" : "a12345678",
     "parent_id" : 22,
     "user_viptime" : "3366-06-06"
   }
