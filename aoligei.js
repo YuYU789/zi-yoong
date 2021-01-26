bfae8e2eae0b177323b8a8c1c65c7b7ecd30316a
yu475487880 <77768638+yu475487880@users.noreply.github.com>
2021-01-26 21:08:05 +0800

    Create aoligei.js

---
+++ aoligei.js
@@ -0,0 +1,33 @@
+var body = {
+  "status": 1,
+  "info": "用户信息",
+  "data": {
+    "user_id": "4583116",
+    "user_name": "183****5028",
+    "password": "cd02386b8cfba1963589e6471396eebf",
+    "mobile": "18310605028",
+    "head_pic": "http://1008610010.yohui.vip/Uploads/Advertisement/20200613_234912_15920633529065_8777.jpg",
+    "disabled": "0",
+    "token": "721224291228306811",
+    "three_party": "0",
+    "cache_num": "1",
+    "open_id": "",
+    "register_time": "1611484169",
+    "last_login_time": "1611484169",
+    "last_login_ip": "59.36.119.251",
+    "share_code": "0",
+    "view_times": "0",
+    "allow_times": "0",
+    "parent_id": "964367",
+    "share_url": "/Uploads/Qrcode/4583116.png",
+    "correspond_uid": "0",
+    "is_can_comments": "1",
+    "user_viptime": "1611484769",
+    "is_daili": "0",
+    "addviptime": "0",
+    "is_vip": 1
+  }
+}
+  
+
+$done({body:JSON.stringify(body)});
