dba5d0e64f54fc043d445a53db115c0cb7783541
YuYU789 <77768638+YuYU789@users.noreply.github.com>
2021-01-31 17:15:25 +0800

    Delete Peiyinxiu.js

--- Peiyinxiu.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-body=body.replace(/level\":\d+/g,'level":0').replace(/is_vip\":\d+/g，'is_vip":1').replace(/code\":\d+/g，'code":0')；
-
-  
-
-$done(body);
-
-
-hostname = iosapi.peiyinxiu.com
